45969b1991824f360236e3f7554a65e980116f3e
Bekian <Bekian.Cat@gmail.com>
2023-10-04 16:13:01 -0500

    See changelog.md

---
+++ changelog.md
@@ -0,0 +1,5 @@
+# Oct. 4th, '23
+    - Cleared template data
+    - Updated template data to project information
+    - Removed Typescript files
+    - Added data provided by filipp999
--- src/lib/stores.js
+++ src/lib/stores.js
@@ -1,13 +1,3 @@
 import { readable } from 'svelte/store';
 
-export const time = readable(new Date(), function start(set) {
-	// setup code goes here
-	const interval = setInterval(() => {
-		set(new Date());
-	}, 1000)
 
-	return function stop() {
-		// teardown code goes here
-		clearInterval(interval)
-	};
-});
